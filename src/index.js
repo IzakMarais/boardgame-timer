148cd343a3e5d5d3207c545cad575cc79ec0ab61
Izak Marais <izak.marais@vastech.co.za>
2018-07-16 10:10:17 +0200

    Timer value configurable from settings. Timer value reset as when player turns cycle back.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import colorsys from 'colorsys';
 
 /*
 TODO
-    index.js:2178 Warning: Can't call setState on a component that is not yet mounted. This is a no-op, but it might indicate a bug in your application. Instead, assign to `this.state` directly or define a `state = {};` class property with the desired state in the App component.
-
-    Animating player colors is too intensive for mobile browser
-
     Pulse faster when time is up? Make sound?
     timer settings
+        optional holdover with max player time that can accumulate
+
     Player names in state
     Style form input
 */
@@ -30,85 +28,57 @@ let DEFAULT_PLAYER_COLORS = [
 ]
 let MIN_PLAYER_COUNT = 2;
 let MAX_PLAYER_COUNT = 8;
+let MIN_TURN_TIME_SEC = 1;
 
-
-class PlayerTimer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            remainingMs: this.props.maxMs
-        }
+function calcTextColor(bgColor) {
+    const rgb = colorsys.parseCss(bgColor);
+    let hsl = colorsys.rgb2Hsl(rgb)
+    let lightness = hsl.l;
+    if (lightness < 50) {
+        lightness = 100;
+    } else {
+        lightness = 0;
     }
+    return colorsys.hsl2Hex({h:0, s:0, l: lightness});
+}
 
-    calcTextColor(bgColor) {
-        const rgb = colorsys.parseCss(bgColor);
-        let hsl = colorsys.rgb2Hsl(rgb)
-        let lightness = hsl.l;
-        if (lightness < 50) {
-            lightness = 100;
-        } else {
-            lightness = 0;
-        }
-        return colorsys.hsl2Hex({h:0, s:0, l: lightness});
+function PlayerTimer(props) {
+    const width = props.remainingMs / props.maxMs * 100;
+    const percentage = width+"%";
+    const remainingSec = Math.ceil(props.remainingMs/1000);
+    const backgroundColor=props.color;
+    let textBoxStyle = {
+        backgroundColor:backgroundColor,
+        color:calcTextColor(backgroundColor),
     }
-
-    tick() {
-        if (this.props.isPaused) {
-            return
-        }
-        if (!this.props.isActive) {
-            return
-        }
-        const activeRemaining = this.state.remainingMs;
-        this.setState({remainingMs:Math.max(0,activeRemaining-TIMESTEP)});
+    let pulseIfActive = {opacity:0};
+    if (props.isActive && !props.isPaused) {
+        pulseIfActive = {animation:"pulse 1s ease-in-out infinite alternate "};
     }
-
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), TIMESTEP);
+    let containerStyle={};
+    if (props.isActive) {
+        containerStyle={flexGrow:1.5};
     }
+    let timerBarStyle = {
+        width:percentage,
+        transition:"width "+TIMESTEP+"ms linear"
+    };
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    render() {
-        const width = this.state.remainingMs / this.props.maxMs * 100;
-        const percentage = width+"%";
-        const remainingSec = Math.ceil(this.state.remainingMs/1000);
-        const backgroundColor=this.props.color;
-        let textBoxStyle = {
-            backgroundColor:backgroundColor,
-            color:this.calcTextColor(backgroundColor),
-        }
-        let pulseIfActive = {opacity:0};
-        if (this.props.isActive && !this.props.isPaused) {
-            pulseIfActive = {animation:"pulse 1s ease-in-out infinite alternate "};
-        }
-        let containerStyle={};
-        if (this.props.isActive) {
-            containerStyle={flexGrow:1.5};
-        }
-        let timerBarStyle = {
-            width:percentage,
-            transition:"width "+TIMESTEP+"ms linear"
-        };
-
-        return (
-            <div className="flex-container" style={containerStyle}>
-                <div className="timer-bar" style={timerBarStyle}/>
-                <CSSTransition
-                    in={this.props.isActive}
-                    timeout={500}
-                    classNames="button-animation"
-                >
-                    <div className="remaining-time" onClick={this.props.onClick} style={textBoxStyle}>
-                        <div className="remaining-time-pulse-darken" style={pulseIfActive}/>
-                        <div className="remaining-time-text">{remainingSec}</div>
-                    </div>
-                </CSSTransition>
-            </div>
-        );
-    }
+    return (
+        <div className="flex-container" style={containerStyle}>
+            <div className="timer-bar" style={timerBarStyle}/>
+            <CSSTransition
+                in={props.isActive}
+                timeout={500}
+                classNames="button-animation"
+            >
+                <div className="remaining-time" onClick={props.onClick} style={textBoxStyle}>
+                    <div className="remaining-time-pulse-darken" style={pulseIfActive}/>
+                    <div className="remaining-time-text">{remainingSec}</div>
+                </div>
+            </CSSTransition>
+        </div>
+    );
 }
 
 function TimerList(props) {
@@ -121,10 +91,11 @@ function TimerList(props) {
         timers.push(
             (<PlayerTimer
                 onClick={() => props.handleClick(i)}
-                maxMs={10000}
+                maxMs={props.turnTimeSec * 1000}
+                remainingMs={props.playerSettings[i].remainingMs}
                 isActive={isActive}
                 isPaused={props.isPaused}
-                color={props.playerColors[i]}
+                color={props.playerSettings[i].color}
                 key={i}
             />));
     }
@@ -201,6 +172,13 @@ function Settings(props) {
                     />
                 </label>
                 <br />
+                <label> Turn time (sec):
+                    <input type="number" min={MIN_TURN_TIME_SEC}
+                        value={props.turnTime}
+                        onChange={props.onTurnTimeChange}
+                    />
+                </label>
+                <br />
                 {colorPickers}
             </form>
         </div>
@@ -216,13 +194,40 @@ class App extends React.Component {
             playerCount: 4,
             playerSettings: [],
             activePlayer:0,
+            turnTimeSec:60,
         }
         for (let i = 0; i < this.state.playerCount; i++) {
-            this.state.playerSettings.push({
-                color: DEFAULT_PLAYER_COLORS[i]
-            });
+            this.state.playerSettings.push(this.newPlayerSettings(i));
         }
     }
+
+    newPlayerSettings(i) {
+        const turnTimeMs = this.state.turnTimeSec * 1000;
+        return {
+            color: DEFAULT_PLAYER_COLORS[i],
+            remainingMs: turnTimeMs,
+        }
+    }
+
+    tick() {
+        if (this.state.isPaused) {
+            return
+        }
+        const i = this.state.activePlayer;
+        const activeRemaining = this.state.playerSettings[i].remainingMs;
+        let playerSettings = this.state.playerSettings.slice();
+        playerSettings[i].remainingMs = Math.max(0,activeRemaining-TIMESTEP);
+        this.setState({playerSettings:playerSettings});
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), TIMESTEP);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
 
     handleTimerListClick(i) {
         //by default clicking on a player makes her active
@@ -231,7 +236,12 @@ class App extends React.Component {
         if (this.state.activePlayer === i) {
             nextActive = (i+1)%this.state.playerCount;
         }
-        this.setState({activePlayer:nextActive});
+        let playerSettings = this.state.playerSettings.slice();
+        playerSettings[nextActive].remainingMs = this.state.turnTimeSec * 1000;
+        this.setState({
+            activePlayer:nextActive,
+            playerSettings:playerSettings,
+        });
     }
 
     handleSettingsPlayerCount(event){
@@ -241,25 +251,35 @@ class App extends React.Component {
 
         let playerSettings = this.state.playerSettings.slice(0, playerCount);
         for (let i = playerSettings.length; i < playerCount; i++) {
-            playerSettings.push({
-                color: DEFAULT_PLAYER_COLORS[i]
-            })
+            playerSettings.push(this.newPlayerSettings(i))
         }
 
         this.setState({
             playerCount:playerCount,
-            playerSettings: playerSettings
+            playerSettings: playerSettings,
+            activePlayer:0
         });
-        this.setActivePlayer(0);
     }
 
     handleSettingsColor(i, event) {
         let playerSettings = this.state.playerSettings.slice()
         playerSettings[i].color = event.target.value;
         this.setState({playerSettings:playerSettings})
-        if (i===this.state.activePlayer) {
-            this.setActivePlayerPulseColors(i);
+    }
+
+    handleSettingsTurnTime(event) {
+        let time = parseFloat(event.target.value);
+        time = Math.max(time, MIN_TURN_TIME_SEC);
+
+        let playerSettings = this.state.playerSettings.slice()
+        for (let i = 0; i < this.state.playerCount; i++) {
+            playerSettings[i].remainingMs = time*1000;
         }
+
+        this.setState({
+            turnTimeSec:time,
+            playerSettings: playerSettings,
+        })
     }
 
     handlSettingsSubmit(event){
@@ -306,7 +326,8 @@ class App extends React.Component {
                     </div>
                     <TimerList
                         playerCount={this.state.playerCount}
-                        playerColors={playerColors}
+                        playerSettings={this.state.playerSettings}
+                        turnTimeSec={this.state.turnTimeSec}
                         isPaused={this.state.isPaused}
                         activePlayer={this.state.activePlayer}
                         handleClick={(i)=>this.handleTimerListClick(i)}
@@ -318,6 +339,8 @@ class App extends React.Component {
                     onPlayerCountChange={(e)=>this.handleSettingsPlayerCount(e)}
                     playerColors={playerColors}
                     onColorChange={(i,e)=>this.handleSettingsColor(i,e)}
+                    turnTime={this.state.turnTimeSec}
+                    onTurnTimeChange={(e)=>this.handleSettingsTurnTime(e)}
                     onSubmit={(e)=>this.handlSettingsSubmit(e)}
                 />
             </React.Fragment>
